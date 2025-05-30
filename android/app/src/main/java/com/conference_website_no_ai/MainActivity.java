59162df3e28afb4099b01a4b1b1cbf0e96c35b53
jali123456 <jali.jel@gmail.com>
2025-05-30 09:50:51 +0800

    readded android folder

---
+++ android/app/src/main/java/com/conference_website_no_ai/MainActivity.java
@@ -0,0 +1,5 @@
+package com.conference_website_no_ai;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.conference_website_no_ai',
+  appName: 'conference_website_no_ai',
+  webDir: 'dist'
+};
+
+export default config;
